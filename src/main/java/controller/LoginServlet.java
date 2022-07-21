7219cc3a446a3c4985ddeb9dc03f1e09674d9b15
Takahide <test@email.com>
2022-07-22 08:55:59 +0900

    loginServlet作成

---
+++ src/main/java/controller/LoginServlet.java
@@ -0,0 +1,65 @@
+package controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dao.DaoFactory;
+import dao.UserDao;
+import domain.User;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("/WEB-INF/view/login.jsp").forward(request, response);	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 入力情報の取得
+		String loginId = request.getParameter("login_id");
+		String loginPass = request.getParameter("login_pass");
+		UserDao userDao = DaoFactory.CreateUserDao();
+		try {
+		User user = userDao.findByLoginIdAndLoginPass(loginId, loginPass);
+		// ユーザーIDとパスワード比較
+		if(user != null) {
+			request.getSession().setAttribute("loginId", user.getLoginId());
+			request.getSession().setAttribute("loginPass", user.getLoginPass());
+			response.sendRedirect("/WEB-INF/view/home.html");
+		} else {
+			request.setAttribute("error", true);
+			request.getRequestDispatcher("/WEB-INF/view/login.jsp").forward(request, response);
+		}
+		} catch (Exception e) {
+			throw new ServletException(e);
+			
+		}
+		// セッションにloginIdを保存
+		// loginDoneにリダイレクト
+		
+
+	}
+
+}
