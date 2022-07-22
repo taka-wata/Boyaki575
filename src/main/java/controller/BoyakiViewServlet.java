d64d00a8461b6d3c1460497cba2d7b9c80c356b0
Takahide <test@email.com>
2022-07-22 13:26:14 +0900

    BoyakiServlet→BoyakiViewServletにリネーム

---
+++ src/main/java/controller/BoyakiViewServlet.java
@@ -0,0 +1,59 @@
+package controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dao.BoyakiDao;
+import dao.DaoFactory;
+import domain.Boyaki;
+
+/**
+ * Servlet implementation class BoyakiServlet
+ */
+@WebServlet("/boyaki")
+public class BoyakiViewServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public BoyakiViewServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		List<Boyaki> boyakiList = new ArrayList<>();
+		try {
+			BoyakiDao boyakiDao = DaoFactory.CreateBoyakiDao();
+			boyakiList = boyakiDao.findAll();
+			request.setAttribute("boyakiList", boyakiList);
+			request.getRequestDispatcher("/WEB-INF/view/boyaki.jsp").forward(request, response);
+		} catch (Exception e) {
+			throw new ServletException(e);
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
