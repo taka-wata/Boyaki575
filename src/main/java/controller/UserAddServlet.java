359af9c831469785cdc92d858617be1a6c89197b
Takahide <test@email.com>
2022-07-22 15:25:36 +0900

    UserAddServlet実装

--- src/main/java/controller/UserAddServlet.java
+++ src/main/java/controller/UserAddServlet.java
@@ -8,34 +8,62 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.mindrot.jbcrypt.BCrypt;
+
+import dao.DaoFactory;
+import dao.UserDao;
+import domain.User;
+
 /**
  * Servlet implementation class UserAddServlet
  */
 @WebServlet("/userAdd")
 public class UserAddServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public UserAddServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public UserAddServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		request.getRequestDispatcher("/WEB-INF/view/userAdd.jsp").forward(request, response);
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		User user = new User();
+		try {
+			user.setLoginId(request.getParameter("loginId"));
+			// PW暗号化
+			String hashed = BCrypt.hashpw(request.getParameter("loginPass"), BCrypt.gensalt());
+			user.setLoginPass(hashed);
+			user.setName(request.getParameter("name"));
+			// UserDao作成
+			UserDao userDao = DaoFactory.CreateUserDao();
+			userDao.insert(user);
+			
+			// ログイン処理
+			request.getSession().setAttribute("loginId", user.getLoginId());
+			request.getSession().setAttribute("name", user.getName());
+
+			request.getRequestDispatcher("/WEB-INF/view/userAddDone.jsp").forward(request, response);
+		} catch (Exception e) {
+			throw new ServletException(e);
+		}
+
 	}
 
 }
