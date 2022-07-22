f4a55b2a3755ec04be137877c3d62f6cf68b4574
Takahide <test@email.com>
2022-07-22 11:01:14 +0900

    BoyakiServlet作成

--- src/main/java/controller/BoyakiServlet.java
+++ src/main/java/controller/BoyakiServlet.java
@@ -1,6 +1,8 @@
 package controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,32 +10,48 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import dao.BoyakiDao;
+import dao.DaoFactory;
+import domain.Boyaki;
+
 /**
  * Servlet implementation class BoyakiServlet
  */
 @WebServlet("/boyaki")
 public class BoyakiServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public BoyakiServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.getRequestDispatcher("/WEB-INF/view/boyaki.jsp").forward(request, response);
+	public BoyakiServlet() {
+		super();
+		// TODO Auto-generated constructor stub
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		List<Boyaki> boyakiList = new ArrayList<>();
+		try {
+			BoyakiDao boyakiDao = DaoFactory.CreateBoyakiDao();
+			boyakiList = boyakiDao.findAll();
+			request.setAttribute("boyakiList", boyakiList);
+			request.getRequestDispatcher("/WEB-INF/view/boyaki.jsp").forward(request, response);
+		} catch (Exception e) {
+			throw new ServletException(e);
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
