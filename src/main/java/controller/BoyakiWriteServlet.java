1021b617231434359309bcdf52996d1102b7f356
Takahide <test@email.com>
2022-07-26 10:01:55 +0900

    バリデーション処理の追加

--- src/main/java/controller/BoyakiWriteServlet.java
+++ src/main/java/controller/BoyakiWriteServlet.java
@@ -19,56 +19,101 @@ import domain.Boyaki;
 @WebServlet("/boyakiWrite")
 public class BoyakiWriteServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public BoyakiWriteServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public BoyakiWriteServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		request.getRequestDispatcher("/WEB-INF/view/writeBoyaki.jsp").forward(request, response);
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		Boyaki boyaki = new Boyaki();
-		// 値の取得
+		Boolean isError = false;
+		// 値の取得・バリデーション処理
 		try {
-		boyaki.setUpper(request.getParameter("upper"));
-		boyaki.setMiddle(request.getParameter("center"));
-		boyaki.setLower(request.getParameter("lower"));
-		
-		// チェックボックスがOnならTrue,それ以外ならfalse
-		String strIsSecret = request.getParameter("isSecret");
-		if (strIsSecret == "on") {
-			boyaki.setIsSecret(true);			
-		} else {
-			boyaki.setIsSecret(false);
-		}
-		UserDao userDao = DaoFactory.CreateUserDao();
-		// セッションからloginIdを取得し、それをもとにidを取得する
-		Integer UserId = userDao.findId((String) request.getSession().getAttribute("loginId"));
-		boyaki.setUserId(UserId);
-		// BoyakiDao作成
-		BoyakiDao boyakiDao = DaoFactory.CreateBoyakiDao();
-		// ぼやきをDBに反映
-		boyakiDao.insert(boyaki);
-		
-		// フォワード
-		request.getRequestDispatcher("/WEB-INF/view/boyakiWriteDone.jsp").forward(request, response);
-		
-		} catch(Exception e) {
+			String upper = request.getParameter("upper");
+			String middle = request.getParameter("middle");
+			String lower = request.getParameter("lower");
+			// 再表示用
+			request.setAttribute("upper", upper);
+			request.setAttribute("middle", middle);
+			request.setAttribute("lower", lower);
+			
+			if (upper.isBlank()) {
+				request.setAttribute("upperErrorMessage", "上の句が未入力です");
+				isError = true;
+			} else if (upper.length() < 4 || upper.length() >= 6) {
+				request.setAttribute("upperErrorMessage", "上の句は4文字以上6文字以内で入力してください。");
+				isError = true;
+			}
+
+			if (middle.isBlank()) {
+				request.setAttribute("middleErrorMessage", "中の句が未入力です");
+				isError = true;
+			} else if (middle.length() < 4 || middle.length() >= 8) {
+				request.setAttribute("middleErrorMessage", "中の句は6文字以上8文字以内で入力してください。");
+				isError = true;
+			}
+
+			if (lower.isBlank()) {
+				request.setAttribute("lowerErrorMessage", "下の句が未入力です");
+				isError = true;
+			} else if (lower.length() < 4 || lower.length() >= 6) {
+				request.setAttribute("lowerErrorMessage", "下の句は4文字以上6文字以内で入力してください。");
+				isError = true;
+			}
+
+			// チェックボックスがOnならTrue,それ以外ならfalse
+			String strIsSecret = request.getParameter("isSecret");
+			if (strIsSecret == "on") {
+				boyaki.setIsSecret(true);
+			} else {
+				boyaki.setIsSecret(false);
+			}
+			// error検出時、処理を中断しフォワード
+			if (isError == true) {
+				request.getRequestDispatcher("/WEB-INF/view/writeBoyaki.jsp").forward(request, response);
+				return;
+			}
+
+			// 値が正常ならboyakiに格納
+			boyaki.setUpper(upper);
+			boyaki.setMiddle(middle);
+			boyaki.setLower(lower);
+			UserDao userDao = DaoFactory.CreateUserDao();
+			// セッションからloginIdを取得し、それをもとにidを取得する
+			Integer UserId = userDao.findId((String) request.getSession().getAttribute("loginId"));
+			boyaki.setUserId(UserId);
+			// BoyakiDao作成
+			BoyakiDao boyakiDao = DaoFactory.CreateBoyakiDao();
+			// ぼやきをDBに反映
+			boyakiDao.insert(boyaki);
+
+			// フォワード
+			request.getRequestDispatcher("/WEB-INF/view/boyakiWriteDone.jsp").forward(request, response);
+
+		} catch (
+
+		Exception e) {
 			throw new ServletException(e);
 		}
-		
+
 	}
 
 }
--- src/main/webapp/WEB-INF/view/writeBoyaki.jsp
+++ src/main/webapp/WEB-INF/view/writeBoyaki.jsp
@@ -1,24 +1,48 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
-<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+	pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Boyaki575</title>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Boyaki575</title>
 </head>
 <body>
-    よしなしごとを５７５でぼやこう！
+	よしなしごとを５７５でぼやこう！
 
-    <form action="" method="post">
-        <p>上の句: <input type="text" name="upper"></p>
-        <p>中の句: <input type="text" name="center"></p>
-        <p>下の句: <input type="text" name="lower"></p>
-        <p><input type="checkbox" name="isSecert">匿名希望</p>
-        <input type="submit" value="ぼやく">
-        <input type="reset" value="クリア">
-    </form>
+	<form action="" method="post">
+		<p>
+			上の句: <input type="text" name="upper" value="${upper}">
+		</p>
+		<c:if test="${not empty upperErrorMessage}">
+			<p>
+				<c:out value="${upperErrorMessage}" />
+			</p>
+		</c:if>
+
+		<p>
+			中の句: <input type="text" name="middle" value="${middle}">
+		</p>
+		<c:if test="${not empty middleErrorMessage}">
+			<p>
+				<c:out value="${middleErrorMessage}" />
+			</p>
+		</c:if>
+		<p>
+			下の句: <input type="text" name="lower" value="${lower}">
+		</p>
+		<c:if test="${not empty lowerErrorMessage}">
+			<p>
+				<c:out value="${lowerErrorMessage}" />
+			</p>
+		</c:if>
+		<p>
+			<input type="checkbox" name="isSecert" >匿名希望
+		</p>
+		<input type="submit" value="ぼやく"> <input type="reset"
+			value="クリア">
+	</form>
 </body>
 </html>
