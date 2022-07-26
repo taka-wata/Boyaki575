ddb8b7ff855454f0f7664965fc7062bfcd31d65f
Takahide <test@email.com>
2022-07-26 09:09:24 +0900

    LoginServletにバリデーション処理を追加

--- src/main/java/controller/LoginServlet.java
+++ src/main/java/controller/LoginServlet.java
@@ -18,48 +18,70 @@ import domain.User;
 @WebServlet("/login")
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public LoginServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.getRequestDispatcher("/WEB-INF/view/login.jsp").forward(request, response);	}
+	public LoginServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// 入力情報の取得
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.getRequestDispatcher("/WEB-INF/view/login.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		Boolean isError = false;
+		// 入力情報の取得・バリデーション
 		String loginId = request.getParameter("login_id");
+		request.setAttribute("loginId", loginId);
+
+		if (loginId.isBlank()) {
+			request.setAttribute("loginIdErrorMessage", "ログインIDが未入力です");
+			isError = true;
+		}
 		String loginPass = request.getParameter("login_pass");
+		if (loginPass.isBlank()) {
+			request.setAttribute("loginPassErrorMessage", "ログインパスワードが未入力です");
+			isError = true;
+		}
+		// ログインID・パスワードが未入力の場合は処理を中断
+		if(isError == true) {
+			request.getRequestDispatcher("/WEB-INF/view/login.jsp").forward(request, response);
+			return;
+		}
+		// ログインID・パスワード入力済の場合、処理を続行
+
 		UserDao userDao = DaoFactory.CreateUserDao();
 		try {
-		User user = userDao.findByLoginIdAndLoginPass(loginId, loginPass);
-		// ユーザーIDとパスワード比較
-		if(user != null) {
-			// セッションにloginId,nameを保存
-			request.getSession().setAttribute("loginId", user.getLoginId());
-			request.getSession().setAttribute("name", user.getName());
-			// boyakiにリダイレクト
-			response.sendRedirect("boyaki");
-		} else {
-			request.setAttribute("error", true);
-			request.setAttribute("errorMessage", "IDまたはパスワードが異なります。");
-			request.getRequestDispatcher("/WEB-INF/view/login.jsp").forward(request, response);
-		}
+
+			User user = userDao.findByLoginIdAndLoginPass(loginId, loginPass);
+			// ユーザーIDとパスワード比較
+			if (user != null) {
+				// セッションにloginId,nameを保存
+				request.getSession().setAttribute("loginId", user.getLoginId());
+				request.getSession().setAttribute("name", user.getName());
+				// boyakiにリダイレクト
+				response.sendRedirect("boyaki");
+			} else {
+				request.setAttribute("errorMessage", "IDまたはパスワードが異なります。");
+				request.getRequestDispatcher("/WEB-INF/view/login.jsp").forward(request, response);
+			}
 		} catch (Exception e) {
 			throw new ServletException(e);
-			
+
 		}
-		
 
 	}
 
